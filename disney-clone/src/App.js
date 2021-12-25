39f290228afd9ca6f9c241d438a824d93a87630d
AndrewSimons3 <simdrew07@gmail.com>
2021-12-25 11:45:31 -0600

    added functionality for signing in and redirecting to home page based on if user is logged in

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
 	return (
 		<div className='App'>
 			<Router>
 				<Header />
-					
-						<Login />
-					
-				
+				<Routes>
+					<Route exact path='/' element={<Login />}></Route>
+					<Route path='/home' element={<Home />}></Route>
+				</Routes>
 			</Router>
 		</div>
 	);
