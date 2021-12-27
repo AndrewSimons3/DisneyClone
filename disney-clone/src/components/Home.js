8ec602324f0dec49953f22b661a6854e51c5d96f
AndrewSimons3 <simdrew07@gmail.com>
2021-12-26 18:09:27 -0600

    added slider component and started view component

--- disney-clone/src/components/Home.js
+++ disney-clone/src/components/Home.js
@@ -1,37 +1,35 @@
 import styled from 'styled-components';
-
-import React from 'react'
+import ImgSlider from './ImgSlider';
+import React from 'react';
+import Viewers from './Viewers';
 
 const Home = () => {
-  return (
+	return (
 		<Container>
-			<h2>hello</h2>
-			<h2>hello</h2>
-			<h2>hello</h2>
-			<h2>hello</h2>
-			<h2>hello</h2>
-			<h2>hello</h2>
+      <ImgSlider />
+      <Viewers />
 		</Container>
 	);
-}
+};
 
 const Container = styled.div`
-  position: relative;
-  background: url('/images/home-background.png');
-  min-height: calc(100vh - 250px);
-  overflow-x: hidden;
-  display: block;
-  top: 70px;
-  padding: 0 calc(3.5vw * 5px);
+	position: relative;
+	background: url('/images/home-background.png');
+	min-height: calc(100vh - 250px);
+	overflow-x: hidden;
+	display: block;
+	top: 70px;
+	padding: 0 calc(3.5vw + 5px);
 
-  &:after {
-    background: url('/images/home-background.png') center center / cover no-repeat fixed;
-    content: '';
-    position: absolute;
-    inset: 0px;
-    opacity: 1;
-    z-index: -1;
-  }
-`
+	&:after {
+		background: url('/images/home-background.png') center center / cover
+			no-repeat fixed;
+		content: '';
+		position: absolute;
+		inset: 0px;
+		opacity: 1;
+		z-index: -1;
+	}
+`;
 
 export default Home;
